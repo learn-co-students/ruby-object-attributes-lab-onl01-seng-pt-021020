9d7dc812698fc35dcf13bc0925ab7ce2de71d35f
Samantha Barros <samantha.barros95@gmail.com>
2020-03-05 17:12:20 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  
+  def name=(their_name)
+    @name = their_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
+  
+  def job
+    @job
+  end
+  
+end
